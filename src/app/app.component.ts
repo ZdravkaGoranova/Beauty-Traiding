1940ba7090d33c69f60a2e777dee8f9c78426d47
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2024-01-04 16:14:48 +0200

    Add new prodject

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/header/header.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import {NavigationComponent  } from "./navigation/navigation.component";
+import {TeamComponent  } from "./team/team.component";
+import { ActivitiesComponent } from "./activities/activities.component";
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    TeamComponent,
+    ActivitiesComponent
+
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'beauty-traiding-app';
+}
