32ef6ebdc0d6942715a5a05a6b90d19bbf0794b7
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2024-01-05 02:02:24 +0200

    Add routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ActivitiesComponent } from "./activities/activities.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import {TeamComponent  } from "./team/team.component";
+import {HeaderComponent  } from "./core/header/header.component";
+
+export const routes: Routes = [
+
+    { path: '', pathMatch: 'full', redirectTo: '/home' },
+    { path: 'home', component: HeaderComponent },
+    { path: 'activities', component: ActivitiesComponent },
+    { path: 'teams', component: TeamComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'contactUs', component: ContactUsComponent },
+
+
+];
