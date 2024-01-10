e4767cddbe231804bad6a5c9f0b96c8723be8411
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2024-01-10 15:16:11 +0200

    ROUTES: makeup CHANGES: add component TIME*: 0h25m

---
+++ src/app/makeup/makeup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-makeup',
+  standalone: true,
+  imports: [],
+  templateUrl: './makeup.component.html',
+  styleUrl: './makeup.component.css'
+})
+export class MakeupComponent {
+
+}
