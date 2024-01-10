1ab95f7a813b4282b2ec2f06045c0f8976c4f363
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2024-01-10 15:46:20 +0200

    ROUTES: barber CHANGES: add componenet TIME*: 0h15m

---
+++ src/app/barber/barber.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barber',
+  standalone: true,
+  imports: [],
+  templateUrl: './barber.component.html',
+  styleUrl: './barber.component.css'
+})
+export class BarberComponent {
+
+}
