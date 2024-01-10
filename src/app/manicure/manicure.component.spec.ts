187077bbbf1137f49928af5a69bb5fd53175c082
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2024-01-10 15:26:19 +0200

    ROUTES: manicure CHANGES: add component  TIME*: 0h15m

---
+++ src/app/manicure/manicure.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManicureComponent } from './manicure.component';
+
+describe('ManicureComponent', () => {
+  let component: ManicureComponent;
+  let fixture: ComponentFixture<ManicureComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ManicureComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ManicureComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/manicure/manicure.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manicure',
+  standalone: true,
+  imports: [],
+  templateUrl: './manicure.component.html',
+  styleUrl: './manicure.component.css'
+})
+export class ManicureComponent {
+
+}
